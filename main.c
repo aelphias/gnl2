2255bd0343aa91e24f10a293514112f18e94ddaa
aelphias <igormitchell11@gmail.com>
2019-10-22 10:36:00 +0300

    /Users/aelphias/gnl_Work/cmake-build-debug/gnl_Work fd3 fd4 aaaaa aaaaa
    (null)
    (null)
    bbbbb
    bbbbb
    
    (null)
    (null)
    (null)
    (null)
    
    Process finished with exit code 0

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: aelphias <aelphias@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/30 14:39:20 by aelphias          #+#    #+#             */
-/*   Updated: 2019/10/21 19:29:16 by aelphias         ###   ########.fr       */
+/*   Updated: 2019/10/22 11:07:19 by aelphias         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,38 @@ int	main(int argc, char **argv)
 {
 	char *line;
 	int fd1;
-	//int fd2;
+	int fd2;
+	int i1;
+	int i2;
 
+
+	i1 = 0;
+	i2 = 0;
 	line = NULL;
-	if (argc != 2)
+	if (argc < 2)
 		puts("Give me something, darling! I want to work)");
 	fd1 = open(argv[1], O_RDONLY);
-	//fd2 = open(argv[2], O_RDONLY);
-	while (get_next_line(fd1, &line))
+	fd2 = open(argv[2], O_RDONLY);
+	while (i1++ < 3)
 	{
+		get_next_line(fd1, &line);
+		printf("%s\n", line);
+		ft_memdel((void **)&line);
+	}
+
+	while (i2++ < 3)
+	{
+		get_next_line(fd2, &line);
+		printf("%s\n", line);
+		ft_memdel((void **)&line);
+	}
+	while (i1++ < 6)
+	{
+		get_next_line(fd1, &line);
 		printf("%s\n", line);
 		ft_memdel((void **)&line);
 	}
 	close(fd1);
-	//close(fd2);
+	close(fd2);
 	return (0);
 }
